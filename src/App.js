eae736499967d591703e1ecd3f6dcbc68b9656d6
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2021-09-27 14:33:24 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Header/Shop/Shop';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
+}
+
+export default App;
